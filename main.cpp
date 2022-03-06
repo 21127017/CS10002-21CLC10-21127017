682ce6b3331ae623badd4d3619b0f9a5f793414a
Gia Bao <21127017@student.hcmus.edu.vn>
2022-03-06 11:20:03 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include "project.h"
+
+int main(){
+
+    int no = 0, semester = 0, year = 0, classroom = -1;
+    profile *pstudent;
+    subjects *psubject;
+    classrooms *pclassid;
+    pstudent = NULL;
+    psubject = NULL;
+    pclassid = NULL;
+
+	while (true){
+		int roll = 1, choice = 1;
+		login(roll);
+		if (roll == 3) break;
+		if (roll == 1){
+			while (choice){
+				print_option_1();
+				cin >> choice;
+				make_choice_1(choice, pstudent, psubject, pclassid, no, classroom, semester, year);
+			}
+		} else {
+			while (choice){
+				print_option_2();
+				cin >> choice;
+				make_choice_2(choice);
+			}
+		}
+	}
+
+	return 0;
+}
