1ba446afed59f5595deb0e08f54c59c76f5e5024
demo1207 <100425282+demo1207@users.noreply.github.com>
2022-03-06 10:53:18 +0700

    Update view_space and login_board

--- project.cpp
+++ project.cpp
@@ -54,23 +54,42 @@ void GoTo(SHORT posX, SHORT posY) {
 	SetConsoleCursorPosition(hStdout, Position);
 }
 
+void view_space() {
+	system("cls");
+	GoTo(0, 0);
+	cout << static_cast<char>(201);
+	for (int i = 0; i < 100; i++)
+		cout << static_cast<char>(205);
+	cout << static_cast<char>(187) << endl;
+	for (int j = 0; j < 50; j++) {
+		cout << static_cast<char>(186);
+		for (int i = 0; i < 100; i++)
+			cout << " ";
+		cout << static_cast<char>(186) << endl;
+	}
+	cout << static_cast<char>(200);
+	for (int i = 0; i < 100; i++)
+		cout << static_cast<char>(205);
+	cout << static_cast<char>(188);
+}
+
 void view_loginboard() {
 	system("cls");
 	GoTo(0, 0);
 
 	//In o login
-	GoTo(22, 8);
+	GoTo(45, 8);
 	cout << static_cast<char>(201);
 	for (int i = 0; i < 11; i++)
 		cout << static_cast<char>(205);
 	cout << static_cast<char>(187);
-	GoTo(22, 9);
+	GoTo(45, 9);
 	cout << static_cast<char>(186);
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 143);
 	cout << "   LOGIN   ";
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 	cout << static_cast<char>(186);
-	GoTo(22, 10);
+	GoTo(45, 10);
 	cout << static_cast<char>(200);
 	for (int i = 0; i < 11; i++)
 		cout << static_cast<char>(205);
